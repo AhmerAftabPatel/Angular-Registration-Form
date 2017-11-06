5290391e219f48763db430f08ef9ca2a0c9d7ae6
Bret Farley <bretfarley12@mittymonarchl.com>
2017-11-06 15:45:27 -0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user = {
+    first:'',
+    last:'',
+    email:'',
+    password:'',
+    adress:'',
+    unit:'',
+    city:'',
+    state:'',
+    luck:''
+  }
+  users = [];
+  onSubmit(){
+    this.users.push(this.user)
+    this.user = {
+      first:'',
+      last:'',
+      email:'',
+      password:'',
+      adress:'',
+      unit:'',
+      city:'',
+      state:'',
+      luck:''
+    }
+  }
 }
